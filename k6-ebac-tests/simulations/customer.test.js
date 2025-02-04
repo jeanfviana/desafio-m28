940b0fe57546bbbd7b41e5973a6ef5cff83bfdb8
Jean Viana <jean.viana@dbccompany.com.br>
2025-02-04 09:46:50 -0300

    Exercício de teste de api com K6

--- k6-ebac-tests/simulations/customer.test.js
+++ k6-ebac-tests/simulations/customer.test.js
@@ -1,7 +1,7 @@
 import { group } from 'k6';
 import Login from '../request/login.request';
 import data from '../data/usuarios.json'
-import User from '../request/user.request';
+import Customer from '../request/customer.request';
 
 export const options = {
   stages: [
@@ -11,21 +11,21 @@ export const options = {
     { duration: '5s', target: 0 }
   ],
   thresholds: {
-    http_req_duration: ['p(50)<1000']
+    http_req_duration: ['p(95) < 1000']
   }
 }
 
 export default function () {
 
   let login = new Login()
-  let user = new User()
+  let customer = new Customer()
 
   group('login and get token', () => {
     login.access(data.usuarioOk.user, data.usuarioOk.pass)
   })
 
-  group('list users', () => {
-    user.list(login.getToken())
+  group('list customers', () => {
+    customer.list(login.getToken())
   })
 
 }
--- k6-ebac-tests/simulations/product.test.js
+++ k6-ebac-tests/simulations/product.test.js
@@ -1,7 +1,7 @@
 import { group } from 'k6';
 import Login from '../request/login.request';
 import data from '../data/usuarios.json'
-import User from '../request/user.request';
+import Product from '../request/product.request';
 
 export const options = {
   stages: [
@@ -11,21 +11,21 @@ export const options = {
     { duration: '5s', target: 0 }
   ],
   thresholds: {
-    http_req_duration: ['p(50)<1000']
+    http_req_duration: ['p(99) < 1000']
   }
 }
 
 export default function () {
 
   let login = new Login()
-  let user = new User()
+  let product = new Product()
 
   group('login and get token', () => {
     login.access(data.usuarioOk.user, data.usuarioOk.pass)
   })
 
-  group('list users', () => {
-    user.list(login.getToken())
+  group('list products', () => {
+    product.list(login.getToken())
   })
 
 }
